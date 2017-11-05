31072f52093c56772703859d42a2e097f9b7ed38
tomo2495 <tomielias5@gmail.com>
2017-11-05 18:53:54 +0100

    Update index-slider.php

--- inc/index-slider.php
+++ inc/index-slider.php
@@ -5,14 +5,14 @@
             <div class="slide-content">
                 <h1>PONUKA SOFTVÉROV PRE 3D GRAFIKU</h1>
                 <p class>Ponuka širokých paliet programov pre NURBS a polygonálne modelovanie.</p>
-                <a href="/pre-3d-modelovanie" class="btn btn-primary btn-lg">Prejsť na ponuku</a>
+                <a href="/softver-pre-3d-modelovanie" class="btn btn-primary btn-lg">Prejsť na ponuku</a>
             </div>
         </div>
         <div class="swiper-slide" style="background-image:url(https://www.academyx.eu/wp-content/uploads/2017/10/2d_grafika-2600x600.jpg);">
             <div class="slide-content">
                 <h1>PONUKA SOFTVÉROV PRE 2D GRAFIKU</h1>
                 <p class>Komplexná ponuka programov pre editáciu rastrovej a vektorovej grafiky.</p>
-                <a href="/pre-2d-grafiku-a-cad" class="btn btn-primary btn-lg">Prejsť na ponuku</a>
+                <a href="/softver-pre-2d-grafiku-a-cad" class="btn btn-primary btn-lg">Prejsť na ponuku</a>
             </div>
         </div>
         <div class="swiper-slide" style="background-image:url(https://www.academyx.eu/wp-content/uploads/2017/10/learning-e1430768333917-2600x600.jpg);">
