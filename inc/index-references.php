a61dfb9930fb4b78992642681cf6bc59eac86367
tomo2495 <tomielias5@gmail.com>
2017-11-05 16:54:17 +0100

    Update index-references.php

--- inc/index-references.php
+++ inc/index-references.php
@@ -8,45 +8,22 @@
 		<div class="row">
 			<div class="col-md-3 col-sm-6">
 				<div class="media_image">
-					<a href="//cms.uniag.sk"><img title="Slovenská poľnohospodárska univerzita v Nitre" src="https://www.academyx.eu/wp-content/uploads/2017/10/SPU-Nitra-Logo.jpg" ></a>
+					<a href="//cms.uniag.sk"><img title="Slovenská poľnohospodárska univerzita v Nitre" alt="Slovenská poľnohospodárska univerzita v Nitre" src="https://www.academyx.eu/wp-content/uploads/2017/10/SPU-Nitra-Logo.jpg" ></a>
 				</div>
 			</div>
 			<div class="col-md-3 col-sm-6">
 				<div class="media_image">
-					<a href="//www.prodom.sk"><img title="PRODOM spol. s r.o." src="https://www.academyx.eu/wp-content/uploads/2017/10/6363.jpg" ></a>
+					<a href="//www.prodom.sk"><img title="PRODOM spol. s r.o." alt="PRODOM spol. s r.o."  src="https://www.academyx.eu/wp-content/uploads/2017/10/6363.jpg" ></a>
 				</div>
 			</div>
 			<div class="col-md-3 col-sm-6" style="">
 				<div class="media_image">
-					<a href="//www.dopravoprojekt.sk"><img title="DOPRAVOPROJEKT, a.s." src="https://www.academyx.eu/wp-content/uploads/2017/10/dopravoprojekt.jpg" ></a>
+					<a href="//www.dopravoprojekt.sk"><img title="DOPRAVOPROJEKT, a.s." alt="DOPRAVOPROJEKT, a.s." src="https://www.academyx.eu/wp-content/uploads/2017/10/dopravoprojekt.jpg" ></a>
 				</div>
 			</div>
 			<div class="col-md-3 col-sm-6">
 				<div class="media_image">
-					<a href="//www.metrostav.sk"><img title="Metrostav SK a.s." src="/wp-content/uploads/2016/03/logo-Metrostav.gif" ></a>
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-md-3 col-sm-6">
-				<div class="media_image">
-					<a href="//www.slovaktelekon.sk"><img title="Slovak Telekom, a. s." src="/wp-content/uploads/2016/03/slovak-telekom.gif" ></a>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-6">
-				<div class="media_image">
-					<a href="//www.itb.sk"><img title="ITB Construction s. r. o." src="/wp-content/uploads/2016/03/logo-ITB.gif" ></a>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-6">
-				<div class="media_image">
-					<a href="//www.stuba.sk"><img title="Slovenská technická univerzita v Bratislave" src="https://www.academyx.eu/wp-content/uploads/2017/10/logo-STU-1.png" ></a>
-				</div>
-			</div>
-			<div class="col-md-3 col-sm-6">
-				<div class="media_image">
-					<a href="//www.jtfg.sk"><img title="J&amp;T Financial Group" src="https://www.academyx.eu/wp-content/uploads/2017/10/JJ-Banka.jpg" ></a>
+					<a href="//www.metrostav.sk"><img title="Metrostav SK a.s." alt="Metrostav SK a.s." src="/wp-content/uploads/2016/03/logo-Metrostav.gif" ></a>
 				</div>
 			</div>
 		</div>
@@ -54,22 +31,45 @@
 		<div class="row">
 			<div class="col-md-3 col-sm-6">
 				<div class="media_image">
-					<a href="//www.vsmu.sk"><img title="Vysoká škola múzických umení v Bratislave" src="/wp-content/uploads/2016/03/logo-VSMU.gif" ></a>
+					<a href="//www.slovaktelekon.sk"><img title="Slovak Telekom, a. s." alt="Slovak Telekom, a. s." src="/wp-content/uploads/2016/03/slovak-telekom.gif" ></a>
 				</div>
 			</div>
 			<div class="col-md-3 col-sm-6">
 				<div class="media_image">
-					<a href="http://www.vsvu.sk/"><img title="Vysoká škola výtvarných umení v Bratislave" src="https://www.academyx.eu/wp-content/uploads/2017/10/logo-VSVU.png" ></a>
+					<a href="//www.itb.sk"><img title="ITB Construction s. r. o." alt="ITB Construction s. r. o." src="/wp-content/uploads/2016/03/logo-ITB.gif" ></a>
 				</div>
 			</div>
 			<div class="col-md-3 col-sm-6">
 				<div class="media_image">
-					<a href="//www.upjs.sk"><img title="Univerzita Pavla Jozefa Šafárika v Košiciach" src="https://www.academyx.eu/wp-content/uploads/2017/10/upjs-logo.jpg" ></a>
+					<a href="//www.stuba.sk"><img title="Slovenská technická univerzita v Bratislave" alt="Slovenská technická univerzita v Bratislave" src="https://www.academyx.eu/wp-content/uploads/2017/10/logo-STU-1.png" ></a>
 				</div>
 			</div>
 			<div class="col-md-3 col-sm-6">
 				<div class="media_image">
-					<a href="//www.sosst.sk/zsse/"><img title="Stredná odborná škola,Stará Turá" src="https://www.academyx.eu/wp-content/uploads/2015/05/logoz.gif" ></a>
+					<a href="//www.jtfg.sk"><img title="J&amp;T Financial Group" alt="J&amp;T Financial Group" src="https://www.academyx.eu/wp-content/uploads/2017/10/JJ-Banka.jpg" ></a>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-3 col-sm-6">
+				<div class="media_image">
+					<a href="//www.vsmu.sk"><img title="Vysoká škola múzických umení v Bratislave" alt="Vysoká škola múzických umení v Bratislave" src="/wp-content/uploads/2016/03/logo-VSMU.gif" ></a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="media_image">
+					<a href="http://www.vsvu.sk/"><img title="Vysoká škola výtvarných umení v Bratislave" alt="Vysoká škola výtvarných umení v Bratislave" src="https://www.academyx.eu/wp-content/uploads/2017/10/logo-VSVU.png" ></a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="media_image">
+					<a href="//www.upjs.sk"><img title="Univerzita Pavla Jozefa Šafárika v Košiciach" alt="Univerzita Pavla Jozefa Šafárika v Košiciach" src="https://www.academyx.eu/wp-content/uploads/2017/10/upjs-logo.jpg" ></a>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<div class="media_image">
+					<a href="//www.sosst.sk/zsse/"><img title="Stredná odborná škola,Stará Turá" alt="Stredná odborná škola,Stará Turá" src="https://www.academyx.eu/wp-content/uploads/2015/05/logoz.gif" ></a>
 				</div>
 			</div>
 		</div>
